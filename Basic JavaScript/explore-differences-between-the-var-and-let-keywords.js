74033cc66bd98f2ee58773b58385451287d63137
Gillian Weisgram <trekker35@gmail.com>
2022-10-03 03:35:55 -0700

    working on lessons again

---
+++ Basic JavaScript/explore-differences-between-the-var-and-let-keywords.js
@@ -0,0 +1,15 @@
+/*
+you can overwrite variable declarations:
+
+var camper = "James";
+var camper = "David";
+console.log(camper); //will return 'David'
+
+JavaScript introduced the 'let' keyword to get around this
+let camper = "James";
+let camper = "David"; //returns an error
+
+*/
+
+let catName = "Oliver";
+let catSound = "Meow!";
