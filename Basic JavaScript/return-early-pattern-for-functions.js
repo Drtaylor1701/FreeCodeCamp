6cba5324c60ad52591b218f8b2cf98dc03c585d2
Gillian Weisgram <trekker35@gmail.com>
2022-11-20 13:07:24 -0800

    did a bunch of exercises

--- Basic JavaScript/return-early-pattern-for-functions.js
+++ Basic JavaScript/return-early-pattern-for-functions.js
@@ -1,16 +1,25 @@
+/*
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+
+The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+*/
+
 // Setup
 function abTest(a, b) {
-    // Only change code below this line
-    if (a < 0) {
-        return undefined;
-    } else if (b < 0) {
-        return undefined;
-    }
+  // Only change code below this line
+  if (a < 0 || b < 0){
+    return undefined;
+  }
 
 
-    // Only change code above this line
+  // Only change code above this line
 
-    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2, 2);
+abTest(2,2);
--- Basic JavaScript/testing-objects-for-properties.js
+++ Basic JavaScript/testing-objects-for-properties.js
@@ -1,9 +1,28 @@
+/*
+
+
+Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
+
+Example
+
+const myObj = {
+  top: "hat",
+  bottom: "pants"
+};
+
+myObj.hasOwnProperty("top");
+myObj.hasOwnProperty("middle");
+
+The first hasOwnProperty returns true, while the second returns false.
+*/
+
 function checkObj(obj, checkProp) {
   // Only change code below this line
   if (obj.hasOwnProperty(checkProp)) {
     return obj[checkProp];
   } else {
     return "Not Found";
-  }
+  };
+
   // Only change code above this line
 }
